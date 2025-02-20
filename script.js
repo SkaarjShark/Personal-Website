a0b7a76155a17d61c64bf72e7047bd5438628282
Jacob Handley <skaarjshark@outlook.com>
2025-02-20 14:43:06 -0600

    More changes on the html script and style files

---
+++ script.js
@@ -0,0 +1,3 @@
+const projectsButton = getElementByClass("projects-button");
+const projectsContainer = getElementByClass("projects-container");
+const headerContainer = getElementByClass("header-container");
