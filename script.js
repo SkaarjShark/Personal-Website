e133e4cace4ab5cd3dd7ccb6b797fabf740bc07f
Jacob Handley <skaarjshark@outlook.com>
2025-04-04 15:03:32 -0500

    Made the explanation next to the project button change dynamically if the projects are displayed or not

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const projectsButton = document.querySelector(".projects-button");
 const projectsContainer = document.querySelector(".projects-container");
-const headerContainer = document.querySelector(".header-container");
+const projectsButtonText = document.querySelector(".projects-button-text");
 
 projectsButton.addEventListener("click", () => {
     if (projectsContainer.classList.contains("hidden")) {
         projectsContainer.classList.remove("hidden");
         projectsButton.textContent = "Hide Projects";
+        projectsButtonText.textContent = "Here are some of my projects I worked on during my time at Bloomtech."
     } else {
         projectsContainer.classList.add("hidden");
         projectsButton.textContent = "Show Projects";
+        projectsButtonText.textContent = "← Click to see some of my projects I worked on during my time at Bloomtech."
     };
 });
 
